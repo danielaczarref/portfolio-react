c7194dc09988883578bb55b0de28ce9e04759d93
danielaczarref <daniela.czarref@gmail.com>
2024-08-10 16:37:01 -0300

    Removing comments

--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -6,8 +6,6 @@ const greeting = "Hello, I am Pete!";
 const bio1 = "A frontend developer";
 const bio2 = "specialised in React";
 
-// Implement the UI for the LandingSection component according to the instructions.
-// Use a combination of Avatar, Heading and VStack components.
 const LandingSection = () => (
   <FullScreenSection
     justifyContent="center"
