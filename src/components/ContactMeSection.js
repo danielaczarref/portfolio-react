ea51c265a285dc5f20b288c72ffb134c05c01e30
danielaczarref <daniela.czarref@gmail.com>
2024-08-10 18:33:24 -0300

    Add loading

--- src/components/ContactMeSection.js
+++ src/components/ContactMeSection.js
@@ -100,7 +100,7 @@ const LandingSection = () => {
                 />
                 <FormErrorMessage>{formik.errors.comment}</FormErrorMessage>
               </FormControl>
-              <Button type="submit" colorScheme="purple" width="full">
+              <Button type="submit" colorScheme="purple" width="full" isLoading={isLoading}>
                 Submit
               </Button>
             </VStack>
